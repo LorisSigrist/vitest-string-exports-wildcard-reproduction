2f2da1663e8efe508f2b64c7acc9c15502d04068
LorisSigrist <loris@sigrist.dev>
2024-06-06 13:37:43 +0200

    Initial comit

---
+++ file-2.js
@@ -0,0 +1,5 @@
+const something = "Something";
+
+// This is valid JS I swear.
+// TS support for this is still very new. Added in https://github.com/microsoft/TypeScript/pull/58640
+export { something as "arbitrary string" };
